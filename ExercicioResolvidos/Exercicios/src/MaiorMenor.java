e887589924e74eed1fe28b6b867c367e9254f9d4
patrickleonardo1277-ai <patrick.leonardo1277@gmail.com>
2026-06-18 19:06:19 -0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	ExercicioResolvidos~origin_main

---
+++ ExercicioResolvidos/Exercicios/src/MaiorMenor.java
@@ -0,0 +1,5 @@
+public class MaiorMenor {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ ExercicioResolvidos/Exercicios/src/SomaMedia.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class SomaMedia {
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        double soma = 0;
+        double[] notas = {7.5, 8.0, 6.5, 9.0, 5.5};
+
+        for (double n : notas) soma+= n;
+        double media = soma/ notas.length;
+
+        System.out.println("Soma = " + soma);
+        System.out.println("Média = " + media);
+
+        int acima_media = 0;
+
+        for (double n : notas){
+            if (n > media) {
+                acima_media++;
+            }
+
+        }
+        System.out.println("As notas acima da média são : " + acima_media);
+
+
+    }
+}
