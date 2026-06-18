7a8f26dc9a6c3f4d538146749e862be89373736b
patrickleonardo1277-ai <patrick.leonardo1277@gmail.com>
2026-06-18 19:55:05 -0300

    Cria projeto de arquivo

---
+++ ExercicioResolvidos/Exercicios/src/Busca.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Busca {
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        int[] v = { 12, 5, 33, 7, 28, 14, 9 };
+        int alvo = 28;
+        int posicao = -1;
+
+        for (int i = 0; i < v.length; i++) {
+            if (v[i] == alvo) {
+                posicao = i;
+                break;
+            }
+        }
+
+        System.out.println(posicao == -1
+                ? "Não encontrado"
+                : "Encontrado em v[" + posicao + "]");
+
+        leitor.close();
+    }
+}
